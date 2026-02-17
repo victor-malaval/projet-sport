978bb1fc5028e26ebe1b9b1ea39e267a04f3bb2e
thomasdepredubois-cmd <thomas.depre.dubois@gmail.com>
2026-02-17 11:51:23 +0100

    classe client

--- src/main/java/client.java
+++ src/main/java/client.java
@@ -7,6 +7,24 @@
  *
  * @author vivic
  */
-class client {
-    
+public class client extends utilisateur {
+    public int numero;
+    public String nom;
+    public String prenom;
+    public int tel;
+    public String adresse;
+    public boolean abonnement;
+
+    public client(String valMDP, String valemail, int valnumero, String valnom, String valprenom, int valtel, String valadresse){
+        super(valMDP, valemail);
+        this.numero = valnumero;
+        this.nom = valnom;
+        this.prenom = valprenom;
+        this.tel = valtel;
+        this.adresse = valadresse;
+        
+    }
+    public void Afficher(){
+        System.out.println(this.MDP+this.email+this.numero+this.nom+this.prenom+this.tel+this.adresse); 
+    }
 }
