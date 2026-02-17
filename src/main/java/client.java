b89653f5ae7b59efa381d1314859854376c8e9dc
thomasdepredubois-cmd <thomas.depre.dubois@gmail.com>
2026-02-17 12:03:51 +0100

    updt client

--- src/main/java/client.java
+++ src/main/java/client.java
@@ -7,6 +7,8 @@
  *
  * @author vivic
  */
+import java.util.ArrayList;
+
 public class client extends utilisateur {
     public int numero;
     public String nom;
@@ -15,16 +17,16 @@ public class client extends utilisateur {
     public String adresse;
     public boolean abonnement;
 
-    public client(String valMDP, String valemail, int valnumero, String valnom, String valprenom, int valtel, String valadresse){
+    public client(String valMDP, String valemail, int valnumero, String valnom, String valprenom, int valtel, String valadresse) {
         super(valMDP, valemail);
         this.numero = valnumero;
         this.nom = valnom;
         this.prenom = valprenom;
         this.tel = valtel;
         this.adresse = valadresse;
-        
     }
-    public void Afficher(){
-        System.out.println(this.MDP+this.email+this.numero+this.nom+this.prenom+this.tel+this.adresse); 
+
+    public void afficher() {
+        System.out.println(this.MDP + " " + this.email + " " + this.numero + " " + this.nom + " " + this.prenom + " " + this.tel + " " + this.adresse);
     }
 }
